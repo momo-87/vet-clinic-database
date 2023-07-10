17ba1d67cb986f95bc16d49756d09091131cf040
Momo-87 <momochristian87@gmail.com>
2023-07-10 12:24:20 +0100

    Create vet_clininc database and create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+my_app=# create database vet_clinic;
+
+my_app=# create table animals(
+my_app(# id INT GENERATED ALWAYS AS IDENTITY,
+my_app(# name VARCHAR(250),
+my_app(# date_of_birth DATE,
+my_app(# escape_attempts INT,
+my_app(# neutered  BOOLEAN,
+my_app(# weoght_kg DECIMAL
+my_app(# );
+
