23c0cfa81a423f75af3b186fa545b1251a5b3982
Momo-87 <momochristian87@gmail.com>
2023-07-11 13:04:16 +0100

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -3,3 +3,6 @@ insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Gabumon', '2018-11-15', 2, true, 8);
 insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Pikachu', '2021-01-07', 1, false, 15.04);
 insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Devimon', '2017-05-12', 5, true, 11);
+
+-- Insert data into owners table
+insert into owners (full_name, age) values ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
